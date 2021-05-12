77497e78a6457c2dfcc5c44a169ded27726b4e63
dmitrii <ffklffkl@gmail.com>
2021-05-12 17:37:11 +0300

    add app config

---
+++ app/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  fileExtensionsWhitelist: [
+    'pdf',
+    'chm',
+    'djvu',
+    'txt',
+    'jpg',
+    'ppt',
+    'pptx',
+    'epub',
+    'rtf',
+    'doc',
+    'docx',
+  ],
+  fileExtensionsBlacklist: [],
+  pathsWhitelist: [],
+  pathsBlacklist: [],
+  logsDir: './logs/',
+  downloadDir: './content/',
+  baseUrl: 'https://doc.lagout.org',
+  linkSelector: 'pre a[href]',
+};
