a0f7737918434a0f3aab106b6a0f0ed0988f6338
dmitrii <ffklffkl@gmail.com>
2021-05-17 01:05:18 +0300

    add config autoloading from cwd

--- app/args.js
+++ app/args.js
@@ -1,45 +1,44 @@
+const path = require('path');
 const config = require('./config');
+
+function autoLoadConfigLookup() {
+  try {
+    return require(path.resolve(process.cwd(), 'grabber.config.json'));
+  } catch (err) {
+    return {};
+  }
+}
 
 module.exports = require('yargs/yargs')(process.argv.slice(2))
   .alias('help', 'h')
   .alias('version', 'v')
   .boolean('force')
-  .default('force', config.force)
   .alias('force', 'f')
   .describe('force', 'Rewrite existing files')
   .string('root-pathname')
-  .default('root-pathname', config.rootPathname)
   .alias('root-pathname', 'r')
   .describe('root-pathname', 'Crawling from this pathname')
   .boolean('write-logs')
-  .default('write-logs', config.writeLogs)
   .alias('write-logs', 'l')
   .describe('write-logs', 'Write logs to file system')
   .array('file-extensions-whitelist')
-  .default('file-extensions-whitelist', config.fileExtensionsWhitelist)
   .alias('file-extensions-whitelist', 'w')
   .describe('file-extensions-whitelist', 'List of allowed extensions')
   .array('file-extensions-blacklist')
-  .default('file-extensions-blacklist', config.fileExtensionsBlacklist)
   .alias('file-extensions-blacklist', 'b')
   .describe('file-extensions-blacklist', 'List of banned extensions')
   .array('paths-whitelist')
-  .default('paths-whitelist', config.pathsWhitelist)
   .describe('paths-whitelist', 'List of allowed paths to crawl')
   .array('paths-blacklist')
-  .default('paths-blacklist', config.pathsBlacklist)
   .describe('paths-blacklist', 'List of banned paths to crawl')
   .string('logs-dir')
-  .default('logs-dir', config.logsDir)
   .describe('logs-dir', 'Output logs directory')
   .string('output-dir')
-  .default('output-dir', config.outputDir)
   .alias('output-dir', 'o')
   .describe('output-dir', 'Directory to uploading files')
   .string('base-url')
-  .default('base-url', config.baseUrl)
   .describe('base-url', 'Url to start crawling')
   .string('link-selector')
-  .default('link-selector', config.linkSelector)
   .describe('link-selector', 'CSS selector for collecting links on a page')
-  .config().argv;
+  .default(config)
+  .config(autoLoadConfigLookup()).argv;
